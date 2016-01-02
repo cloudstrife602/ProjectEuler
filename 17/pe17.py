56cc2e0264d6a9ad6c619d069c483cbec7b3329e
James Monroe <cloudstrife602@gmail.com>
2016-01-01 17:52:58 -0800

    PE17: Added tests

--- 17/pe17.py
+++ 17/pe17.py
@@ -1,4 +1,23 @@
 import inflect
+import unittest
+
+
+class TestPE17Methods(unittest.TestCase):
+
+    def test_numToWords(self):
+        number = 342
+        self.assertEqual(numToWords(number), "three hundred and forty-two")
+        number = 115
+        self.assertEqual(numToWords(number), "one hundred and fifteen")
+
+    def test_stringClean(self):
+        self.assertEqual(stringClean("three hundred and forty-two"), "threehundredandfortytwo")
+        self.assertEqual(stringClean("one hundred and fifteen"), "onehundredandfifteen")
+
+    def test_numberLetterCount(self):
+        self.assertEqual(numberLetterCount(342), 23)
+        self.assertEqual(numberLetterCount(115), 20)
+        
 p = inflect.engine()
 
 
@@ -15,10 +34,6 @@ def stringClean(string):
 def numberLetterCount(num):
     return len(stringClean(numToWords(num)))
 
-# number = 115
-# print(numToWords(number))
-# print(stringClean(numToWords(number)))
-# print(numberLetterCount(number))
 
 sum = 0
 for i in range(1, 1001):
